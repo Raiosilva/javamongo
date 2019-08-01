d06521232d4772e507a8dfa4ceb2ea50b391f79a
Raiosilva <raimundopi025@gmail.com>
2019-08-01 11:05:20 -0300

    Obtendo post por id

---
+++ src/main/java/com/oliveira/javamongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.oliveira.javamongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.oliveira.javamongo.domain.Post;
+import com.oliveira.javamongo.repository.PostRepository;
+import com.oliveira.javamongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+	}
+}
