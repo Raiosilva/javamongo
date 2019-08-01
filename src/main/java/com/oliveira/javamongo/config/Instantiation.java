d6788a269ba044f30b96d4c85b1f4a4aad28d3fa
Raiosilva <raimundopi025@gmail.com>
2019-08-01 11:16:54 -0300

    Adicionando comentarios ao posts

--- src/main/java/com/oliveira/javamongo/config/Instantiation.java
+++ src/main/java/com/oliveira/javamongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.oliveira.javamongo.domain.Post;
 import com.oliveira.javamongo.domain.User;
 import com.oliveira.javamongo.dto.AuthorDTO;
+import com.oliveira.javamongo.dto.CommentDTO;
 import com.oliveira.javamongo.repository.PostRepository;
 import com.oliveira.javamongo.repository.UserRepository;
 
@@ -46,6 +47,12 @@ public class Instantiation implements CommandLineRunner {
 				sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", 
 				new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viajem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
--- src/main/java/com/oliveira/javamongo/domain/Post.java
+++ src/main/java/com/oliveira/javamongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.oliveira.javamongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.oliveira.javamongo.dto.AuthorDTO;
+import com.oliveira.javamongo.dto.CommentDTO;
 
 
 @Document
@@ -20,6 +23,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	
 	public Post() {
@@ -75,6 +80,15 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -99,6 +113,6 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 
 }
